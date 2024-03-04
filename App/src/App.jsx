744b8b4f621ecc682c70bad7152084ea7d168b02
rodgerswisdom <trulyhawona@gmail.com>
2024-03-04 19:25:50 +0300

    Landingpage poto, index.html, src

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Iko Swali?</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
