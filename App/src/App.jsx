4facb8a983859ffe804a10f03df527c2d9a90f3d
rodgerswisdom <trulyhawona@gmail.com>
2024-03-04 19:59:46 +0300

    Landing page

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import LandingPageSection from './components/landingPage'
+
 
 function App() {
   
   return (
-    <>
-     
-       
+    < >
+    <div>
+     <LandingPageSection className="w-full h-full bg-black"/>
+    </div>
     </>
   )
 }
--- App/src/components/landingPage.jsx
+++ App/src/components/landingPage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import mainImage from "../assets/images/landing_page.jpg"
 
 
 export default function LandingPageSection(){
     return(
         <>
-            <div className="main">
-                <img src="../assets/landing_page.jpg" alt="main-image" />
+            <div className="w-full h-full flex justify-center items-center bg-black" >
+                <img src={mainImage} alt="main-image" className="object-cover" />
             </div>
         </>
     );
