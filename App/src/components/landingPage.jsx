3d77e39346411896d58207f141663bcb07ab5a04
rodgerswisdom <trulyhawona@gmail.com>
2024-03-04 19:35:15 +0300

    main image

--- App/src/components/landingPage.jsx
+++ App/src/components/landingPage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
+
 export default function LandingPageSection(){
     return(
         <>
             <div className="main">
-                
+                <img src="../assets/landing_page.jpg" alt="main-image" />
             </div>
         </>
     );
