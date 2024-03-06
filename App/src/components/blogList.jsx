87732e99db121746f96e14a2b42876537c5aa99e
rodgerswisdom <trulyhawona@gmail.com>
2024-03-06 19:24:09 +0300

    Feature blogs section

--- App/src/components/blogList.jsx
+++ App/src/components/blogList.jsx
@@ -1,11 +1,14 @@
 const BlogList = ()=>{
     return(
         <div className="featured">
-            <div className="list">
+            <div className="image">
                 <img
                     src={}
                     alt = "corn seeds"
-                />
+                /> 
+            </div>
+            <div className="highlight">
+                <h2>DK 8031, 8033, 777 and H6213. Which one should you grow in a high altitude area?</h2> 
             </div>
         </div>
     );
