4bd7666dab084f52dc4aeded24a7c1f22efd8f29
rodgerswisdom <trulyhawona@gmail.com>
2024-03-06 18:59:30 +0300

    landing page image added

--- App/src/components/landingPage.jsx
+++ App/src/components/landingPage.jsx
@@ -1,10 +1,14 @@
-
+import landingPageImage from '../assets/images/landing_page.jpg'
 
 export default function LandingPageSection(){
     return(
         
             <div  className="w-full h-full bg-black">
-                
+                <img
+                    src={landingPageImage}
+                    alt = "landpage"
+                    width={'100%'}
+                />
             </div>
         
     );
