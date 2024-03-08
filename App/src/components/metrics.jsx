50cf9e28d1c40512de65cde85f4dc5ac5fa2b8c9
rodgerswisdom <trulyhawona@gmail.com>
2024-03-09 02:04:58 +0300

    Metrics

--- App/src/components/metrics.jsx
+++ App/src/components/metrics.jsx
@@ -1,6 +1,6 @@
 function Metrics(){
     return(
-        <div class="flex flex-nowrap p-24 gap-32 text-center">
+        <div class="flex flex-nowrap p-24 gap-32 text-center w-full">
             <div>01</div>
             <div>02</div>
             <div>03</div>
