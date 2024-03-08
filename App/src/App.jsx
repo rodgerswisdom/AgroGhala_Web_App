0e342d6171698129c2c513ce246905cddf3cb0e4
rodgerswisdom <trulyhawona@gmail.com>
2024-03-09 02:02:18 +0300

    Metrics

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     <>
      <LandingPageSection />
      <BlogList />
+     <
     </>
   )
 }
---
+++ App/src/components/metrics.jsx
@@ -0,0 +1,11 @@
+function Metrics(){
+    return(
+        <div class="flex flex-nowrap">
+            <div>01</div>
+            <div>02</div>
+            <div>03</div>
+        </div>
+    )
+}
+
+export default Metrics;
