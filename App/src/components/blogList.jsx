f93f67510123ede1e49eff07e7fcd136cc71806b
rodgerswisdom <trulyhawona@gmail.com>
2024-03-09 01:40:52 +0300

    Spacer

--- App/src/components/blogList.jsx
+++ App/src/components/blogList.jsx
@@ -1,4 +1,5 @@
 import imageUrl from "../assets/images/fertilizer_pack.webp";
+import Spacer from "./spacer";
 
 const BlogList = ()=>{
 
@@ -13,9 +14,11 @@ const BlogList = ()=>{
                         alt = "corn seeds"
                     /> 
                 </div>
+               
                 <div className="highlight bg-green-100 absolute  h-24 p-2 m-2" >
                     <h2>DK 8031, 8033, 777 and H6213. Which one should you grow in a high altitude area?</h2> 
                 </div>
+                <Spacer />
                 <div className="image">
                     <img
                         src={imageUrl}
