297a12153c08fe0c05271bea86ed6fa9743a0702
rodgerswisdom <trulyhawona@gmail.com>
2024-03-09 01:21:46 +0300

    blog_list

--- App/src/components/landingPage.jsx
+++ App/src/components/landingPage.jsx
@@ -3,21 +3,25 @@ import landingPageImage from '../assets/images/landing_page.jpg'
 export default function LandingPageSection(){
     return(
         <>
-        <h1>Site under Development</h1>
-            <div  className="w-full h-full bg-black relative">
+<h1>Site under Development</h1>
+       <div  className="w-full h-full bg-black relative">
                 <img
                     src={landingPageImage}
                     alt = "landpage"
                     width={'100%'}
+                    height={'100%'}
                 />
-                <div  className="w-full h-full bg-white static text-center">
-                    <div className='text-green-500'>
-                        <h1>Jamii Ya Wakulima shupavu</h1>
-                        <h2>Mafunzo ya kuboresha kilimo</h2>
+                <div  className="absolute top-0 left-0 w-full h-full bg-green-600 opacity-20"></div>
+                <div className="absolute top-0 left-0 w-3/5 h-full bg-white transform origin-top-left -skew-x-12 text-center ">
+                    <h1 className='text-black'><b>Jamii Ya </b><b className='text-green-600'>Wakulima shupavu</b></h1>
+                    <h2 className='text-green-600'><b>Mafunzo </b><b className='text-black'>ya kuboresha kilimo</b></h2>
+
+                    {/* landing page button */}
+                    <div >
+                        <button className=' bg-green-600 w-3/4'>Je, una swali? Uliza na utajibiwa</button>
                     </div>
                 </div>
             </div>
-            
             </> 
         
     );
