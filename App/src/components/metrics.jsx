ae8722e271506aba97bbcb607367bd690f40f0ba
rodgerswisdom <trulyhawona@gmail.com>
2024-03-09 08:24:24 +0300

    blog_liat

--- App/src/components/metrics.jsx
+++ App/src/components/metrics.jsx
@@ -1,9 +1,9 @@
 function Metrics(){
     return(
         <div class="flex flex-nowrap p-24 gap-32 text-center w-full">
-            <div>01</div>
-            <div>02</div>
-            <div>03</div>
+            <div className="w-[60px] h-[60px] bg-green-500 rounded-full flex justify-center items-center"> 01</div>
+            <div className="w-[60px] h-[60px] bg-green-500 rounded-full flex justify-center items-center">02</div>
+            <div className="w-[60px] h-[60px] bg-green-500 rounded-full flex justify-center items-center">03</div>
         </div>
     )
 }
