786e4c59525fdcc9ef923a8a27e00dfe122c8ad2
rodgerswisdom <trulyhawona@gmail.com>
2024-03-12 18:59:55 +0300

    Quality Assurance

--- App/src/components/metrics.jsx
+++ App/src/components/metrics.jsx
@@ -1,5 +1,6 @@
 function Metrics(){
     return(
+        <>
        <div className="flex  gap-10 m-10 p-10 justify-center text-center w-full">
             <div className=" w-96 h-96 bg-green-600 m-10 p-10">
             <h1 className="text-black text-9xl">400</h1>
@@ -13,7 +14,12 @@ function Metrics(){
             <h1 className="text-black text-9xl">250</h1>
                 <h2 className=" text-6xl text-black">Summary answersby AgroGhala</h2>
             </div>
+            
        </div>
+       <div className=" text-center justify-center">
+       <h1>We work with a team of experts to guarantee Quality</h1>
+       </div>
+       </>
     )
 }
 
