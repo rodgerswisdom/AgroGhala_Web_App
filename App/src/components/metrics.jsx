08335faaff9304fb5d6e9f093c9663ab4bb8b83c
rodgerswisdom <trulyhawona@gmail.com>
2024-03-12 18:46:46 +0300

    Metrics

--- App/src/components/metrics.jsx
+++ App/src/components/metrics.jsx
@@ -1,10 +1,19 @@
 function Metrics(){
     return(
-        <div class="flex flex-nowrap p-24 gap-32 text-center w-full">
-            <div className="w-[60px] h-[60px] bg-green-500 rounded-full flex justify-center items-center"> 01</div>
-            <div className="w-[60px] h-[60px] bg-green-500 rounded-full flex justify-center items-center">02</div>
-            <div className="w-[60px] h-[60px] bg-green-500 rounded-full flex justify-center items-center">03</div>
-        </div>
+       <div className="flex  gap-10 m-10 p-10 justify-center text-center w-full">
+            <div className=" w-96 h-96 bg-green-600 m-10 p-10">
+            <h1 className="text-black text-9xl">400</h1>
+                <h2>Questions by farmers</h2>
+            </div>
+            <div className=" w-96 h-96 bg-green-600 m-10 p-10">
+                <h1 className="text-black text-9xl">1200</h1>
+                <h2>Answers by farmers</h2>
+            </div>
+            <div className=" w-96 h-96 bg-green-600 p-10 m-10">
+            <h1 className="text-black text-9xl">250</h1>
+                <h2>Summary answersby AgroGhala</h2>
+            </div>
+       </div>
     )
 }
 
