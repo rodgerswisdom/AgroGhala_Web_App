292d99ebc5ec62e73eee99f8809018acf1b355f2
rodgerswisdom <trulyhawona@gmail.com>
2024-03-12 18:51:39 +0300

    Metrics details

--- App/src/components/metrics.jsx
+++ App/src/components/metrics.jsx
@@ -3,15 +3,15 @@ function Metrics(){
        <div className="flex  gap-10 m-10 p-10 justify-center text-center w-full">
             <div className=" w-96 h-96 bg-green-600 m-10 p-10">
             <h1 className="text-black text-9xl">400</h1>
-                <h2>Questions by farmers</h2>
+                <h2 className=" text-6xl text-black">Questions by farmers</h2>
             </div>
             <div className=" w-96 h-96 bg-green-600 m-10 p-10">
                 <h1 className="text-black text-9xl">1200</h1>
-                <h2>Answers by farmers</h2>
+                <h2 className=" text-6xl text-black">Answers by farmers</h2>
             </div>
             <div className=" w-96 h-96 bg-green-600 p-10 m-10">
             <h1 className="text-black text-9xl">250</h1>
-                <h2>Summary answersby AgroGhala</h2>
+                <h2 className=" text-6xl text-black">Summary answersby AgroGhala</h2>
             </div>
        </div>
     )
